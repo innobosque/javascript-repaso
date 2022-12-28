719268ae744f31166a402c81da6dff1afe822ade
xurxogonzalez <xurxo@webferrol.com>
2022-12-28 12:28:02 +0100

    ejercicio 5

--- js/ej3_ej4.js
+++ js/ej3_ej4.js
@@ -1,8 +1,3 @@
-/**
- * Ej. Escribir un programa en el cual se ingresen cuatro números, calcular e informar la suma de los dos primeros y el producto del tercero y el cuarto.
- *  
- */
-
 
 const esNumero = (textoValor) => {
     if(textoValor!=="" && textoValor!==null && !isNaN(textoValor)){
@@ -12,10 +7,18 @@ const esNumero = (textoValor) => {
     }
 }
 
+
 const alerta= (parametro) => {
     let mensaje = `[${parametro}] no es un número válido`;
     return mensaje;
 }
+
+
+/**
+ * Ej.3 Escribir un programa en el cual se ingresen cuatro números, calcular e informar la suma de los dos primeros y el producto del tercero y el cuarto.
+ *  
+ */
+
 
 const b3 = document.querySelector("#b_ej3");
 
@@ -47,4 +50,74 @@ b3.onclick = () => {
     }else{
         alert(alerta(n1));   
     }
+}
+
+/**
+ * 5. Realizar un programa que lea cuatro valores numéricos enteros y cuente cuántos son positivos o negativos.
+ */
+//Capturar el botón
+
+document.querySelector("#b_ej5").onclick = () => {
+   let pos = neg = cero = 0;
+   let num1 = prompt("Número 1");
+   let num2 = prompt("Numero 2");
+   let num3 = prompt("Número 3");
+   let num4 = prompt("Número 4");
+
+   if(esNumero(num1) && esNumero(num2) && esNumero(num3) && esNumero(num4)){
+    //Convierto los string en number
+    num1 = Number(num1);
+    num2 = Number(num2);
+    num3 = Number(num3);
+    num4 = Number(num4);
+    //Mirar is num1 es negativo positivo o cero
+    if(num1>0){
+        pos++; //pos = pos +1
+    }else if(num1<0){
+       neg++; 
+    }else{
+        cero++;
+    }
+
+    //Mirar is num2 es negativo positivo o cero
+    if(num2>0){
+        pos++; //pos = pos +1
+    }else if(num2<0){
+       neg++; 
+    }else{
+        cero++;
+    }
+
+    //Mirar is num3 es negativo positivo o cero
+    if(num3>0){
+        pos++; //pos = pos +1
+    }else if(num3<0){
+       neg++; 
+    }else{
+        cero++;
+    }
+
+    //Mirar is num4 es negativo positivo o cero
+    if(num4>0){
+        pos++; //pos = pos +1
+    }else if(num4<0){
+       neg++; 
+    }else{
+        cero++;
+    }
+
+
+    //Mostrar resultados
+    document.getElementById("r_ej5").innerHTML = 
+    `<p>[${num1},${num2},${num3},${num4}]: Positivos: ${pos}, Negativos: ${neg}, Ceros: ${cero}</p>`
+
+
+
+
+   }else{
+     alert(`Todos los valores tienen que ser numéricos:\n ${num1},${num2},${num3},${num4}`);
+   }
+
+
+
 }
